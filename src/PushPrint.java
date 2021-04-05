49512b14fa8652a393b4492d4bb9e6b43e9d4992
dhkim <mnbv980210@naver.com>
2021-04-06 01:08:24 +0900

    Update Push Print.java in Local

--- src/PushPrint.java
+++ src/PushPrint.java
@@ -2,8 +2,8 @@
 public class PushPrint {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Add java file");
+		System.out.println("Hello in Lcocal....");
 	}
 
 }
